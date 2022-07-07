9600610ac30141baccae678f74387d055a2dbe1e
cudillal <cudilla.leopold@hotmail.com>
2022-07-07 08:24:38 +0000

    Normalize synthetic data rd and hp names to match with the embedding keys

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from dp_cgans import DP_CGAN
 import pandas as pd
+import pickle as pkl
 
 tabular_data = pd.read_csv("../persistent/data/syn_data/syn_patients_data_unseen_names.txt", names=["patient_id", "rare_disease", "phenotype"])
 
+with open('../persistent/data/ontology/embeddings/Onto_TransE.pkl', 'rb') as f:
+    embeds = pkl.load(f)
+    print(dict(list(embeds.items())[0:10]))
+
 # We adjusted the original CTGAN model from SDV. Instead of looking at the distribution of individual variable, we extended to two variables and keep their corrll
-model = Onto_DP_CGAN(
-    entity_embed_file='../persistent/data/ontology/save_onto_embeds/entity_500000.npy',
-    rel_embed_file='../persistent/data/ontology/save_onto_embeds/relation_500000.npy',
+"""model = Onto_DP_CGAN(
+    embeddings_path='../persistent/data/ontology/embeddings/Onto_TransE.pkl',
+    data_path='../persistent/data/ontology/save_onto_embeds/relation_500000.npy',
     epochs=2, # number of training epochs
     batch_size=1000, # the size of each batch
     log_frequency=True,
@@ -17,10 +22,10 @@ model = Onto_DP_CGAN(
     discriminator_lr=2e-4,
     discriminator_steps=1,
     private=False,
-)
+)"""
 
 print("Start training model")
-model.fit(tabular_data)
+# model.fit(tabular_data)
 
 # Sample the generated synthetic data
-model.sample(100)
+# model.sample(100)
